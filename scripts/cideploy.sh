ae9963656e1739ca70107072c619bf3e73ffa4e5
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-12-14 22:47:08 +0530

    Use rsync to push deployment
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/cideploy.sh
+++ scripts/cideploy.sh
@@ -2,5 +2,7 @@
 set -e
 
 sshpass -p "$DEPLOY_PASS" \
-  scp -r _site/ \
+  rsync -drvz --delete --delete-before --delete-excluded --force --stats \
+  -e "ssh -o StrictHostKeyChecking=no" \
+  _site/ \
   "$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH"
