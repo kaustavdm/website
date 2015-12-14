ae70c5bb26af146e81df7173487bdc902c6ba0a6
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-12-14 17:11:30 +0530

    Add verbose mode to jekyll and htmlproofer runs
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build
-bundle exec htmlproof ./_site --disable-external --only-4xx --allow-hash-href --check-html --directory-index-file "index.html"
+bundle exec jekyll build --verbose
+bundle exec htmlproof ./_site --verbose --disable-external --only-4xx --allow-hash-href --check-html --directory-index-file "index.html"
