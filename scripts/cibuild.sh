0acece1db99a68f0bd619f671284be3e70728bb3
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-12-14 21:01:39 +0530

    Reformat scripts/cibuild.sh with better indentation
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-set -e # halt script on error
+set -e
 
 bundle exec jekyll build --verbose
-bundle exec htmlproof ./_site --verbose --disable-external --only-4xx --allow-hash-href --check-html --directory-index-file "index.html"
+
+bundle exec htmlproof ./_site \
+  --verbose --disable-external --only-4xx --allow-hash-href \
+  --check-html --directory-index-file "index.html"
