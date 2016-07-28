7ea233506341a49b1c1f3a55642a99e715d58c47
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-07-28 14:15:30 +0530

    Copy htaccess as part of build
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,6 +3,8 @@ set -e
 
 bundle exec jekyll build
 
+cp -vf ./scripts/_.htaccess ./_site/.htaccess
+
 bundle exec htmlproof ./_site \
   --disable-external --only-4xx --allow-hash-href \
   --check-html --directory-index-file "index.html"
