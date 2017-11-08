bfdfe9d87bfbc1ecbb6e74bc9bdd45610a7870a4
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-11-09 00:10:55 +0530

    Remove old build scripts
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/cideploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-sshpass -p "$DEPLOY_PASS" \
-  rsync -drz --delete --delete-before --delete-excluded --force --stats \
-  -e "ssh -o StrictHostKeyChecking=no" \
-  _site/ \
-  "$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH"
